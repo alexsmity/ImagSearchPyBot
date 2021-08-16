3aed7cd45e163e1bca766ef5880bba03b3cd7e8f
alex smith <88277919+alexsmity@users.noreply.github.com>
2021-08-16 16:14:50 +0300

    Update main.py

--- img_sh_py_bot/main.py
+++ img_sh_py_bot/main.py
@@ -2,7 +2,6 @@ import discord
 import os
 from discord.ext import commands
 from googleapiclient.discovery import build
-##from keep_alive import keep_alive
 
 client = commands.Bot(command_prefix=".")#bot prefix example: (.show)
 
@@ -17,5 +16,4 @@ for filename in os.listdir("./cogs"):
     if filename.endswith(".py"):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-#keep_alive()
 client.run('your boy token')
